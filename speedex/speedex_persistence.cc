a5ee4dc274a6ad51b04ad07aea289172bfdb1734
gramseyer <ramseyer@uchicago.edu>
2021-06-24 18:32:55 -0700

    persistence code

---
+++ speedex/speedex_persistence.cc
@@ -0,0 +1,157 @@
+#include "speedex/speedex_persistence.h"
+
+#include "utils/header_persistence.h"
+
+namespace speedex {
+
+std::unique_ptr<AccountModificationBlock>
+persist_critical_round_data(
+	SpeedexManagementStructures& management_structures,
+	const HashedBlock& header,
+	BlockDataPersistenceMeasurements& measurements,
+	bool get_block,
+	uint64_t log_offset) {
+
+	auto timestamp = init_time_measurement();
+
+	save_header(header);
+
+	measurements.header_write_time = measure_time(timestamp);
+
+	uint64_t current_block_number = header.block.blockNumber;
+
+	auto block_out = management_structures
+		.account_modification_log
+		.persist_block(current_block_number + log_offset, get_block);
+
+	BLOCK_INFO("done writing account log");
+	measurements.account_log_write_time = measure_time(timestamp);
+
+	management_structures.db.add_persistence_thunk(
+		current_block_number, management_structures.account_modification_log);
+
+	measurements.account_db_checkpoint_time = measure_time(timestamp);
+
+	management_structures.account_modification_log.detached_clear();
+	BLOCK_INFO("done persist critical round data");
+	//offer db thunks, header hash map already updated
+	return block_out;
+}
+
+void persist_async_phase1(
+	SpeedexManagementStructures& management_structures,
+	const uint64_t current_block_number,
+	BlockDataPersistenceMeasurements& measurements) {
+
+	BLOCK_INFO("starting async persistence");
+	auto timestamp = init_time_measurement();
+
+	management_structures.db.commit_persistence_thunks(current_block_number);
+	BLOCK_INFO("done async db persistence\n");
+	measurements.account_db_checkpoint_finish_time = measure_time(timestamp);
+}
+
+void 
+AsyncPersister::run() {
+	while (true) {
+		std::unique_lock lock(mtx);
+		if ((!exists_work_to_do()) && (!done_flag)) {
+			cv.wait(lock, [this] { return done_flag || exists_work_to_do();});
+		}
+		if (done_flag) return;
+		persist_async_phase1(
+			management_structures, 
+			*block_number_to_persist, 
+			*latest_measurements);
+
+		phase2_persist.do_async_persist_phase2(
+			*block_number_to_persist, latest_measurements);
+
+		highest_persisted_block = *block_number_to_persist;
+
+		block_number_to_persist = std::nullopt;
+		latest_measurements = nullptr;
+		cv.notify_one();
+	}
+}
+
+void
+persist_async_phase2(
+	SpeedexManagementStructures& management_structures,
+	uint64_t current_block_number,
+	BlockDataPersistenceMeasurements& measurements) {
+
+	BLOCK_INFO("starting async persistence phase 2");
+	auto timestamp = init_time_measurement();
+
+	management_structures.db.force_sync();
+	BLOCK_INFO("done async db sync\n");
+	measurements.account_db_checkpoint_sync_time = measure_time(timestamp);
+}
+
+void 
+AsyncPersisterPhase2::run() {
+	while(true) {
+		std::unique_lock lock(mtx);
+		if ((!done_flag) && (!exists_work_to_do())) {
+			cv.wait(lock, [this] () { return done_flag || exists_work_to_do();});
+		}
+		if (done_flag) return;
+		if (latest_measurements == nullptr) {
+			throw std::runtime_error("invalid call to async_persist_phase2!");
+		}
+
+		persist_async_phase2(
+			management_structures, 
+			*current_block_number, 
+			*latest_measurements);
+		
+		phase3_persist.do_async_persist_phase3(
+			*current_block_number, latest_measurements);
+
+		latest_measurements = nullptr;
+		current_block_number = std::nullopt;
+		cv.notify_all();
+	}
+}
+
+void
+persist_async_phase3(
+	SpeedexManagementStructures& management_structures,
+	uint64_t current_block_number,
+	BlockDataPersistenceMeasurements& measurements) {
+
+	BLOCK_INFO("starting async persistence phase 3");
+	auto timestamp = init_time_measurement();
+
+	management_structures.orderbook_manager.persist_lmdb(current_block_number);
+	BLOCK_INFO("done async offer persistence\n");
+	measurements.offer_checkpoint_time = measure_time(timestamp);
+
+	management_structures.block_header_hash_map.persist_lmdb(
+		current_block_number);
+
+	measurements.block_hash_map_checkpoint_time = measure_time(timestamp);
+	BLOCK_INFO("done async total persistence\n");
+}
+
+void 
+AsyncPersisterPhase3::run() {
+	while(true) {
+		std::unique_lock lock(mtx);
+		if ((!done_flag) && (!exists_work_to_do())) {
+			cv.wait(lock, [this] () { return done_flag || exists_work_to_do();});
+		}
+		if (done_flag) return;
+		if (latest_measurements == nullptr) {
+			throw std::runtime_error("invalid call to async_persist_phase3!");
+		}
+		persist_async_phase3(
+			management_structures, *current_block_number, *latest_measurements);
+		latest_measurements = nullptr;
+		current_block_number = std::nullopt;
+		cv.notify_all();
+	}
+}
+
+} /* speedex */
--- speedex/speedex_persistence.h
+++ speedex/speedex_persistence.h
@@ -1,40 +1,60 @@
 #pragma once
 
+/*! \file speedex_persistence.h
+
+Asynchronously persist data to disk.
+
+Phase 0 (critical round data): transaction block & header to disk, and snapshots
+account database to memory. Need to run this before we can send a block out to 
+other nodes.
+
+Phase 1: update account database lmdb with new account balances.
+Phase 2: Force an msync of the account database lmdb to disk.
+Phase 3: Everything else (orderbooks, header hash)
+*/
+
+#include <cstdint>
+
+#include "speedex/speedex_management_structures.h"
+
+#include "utils/async_worker.h"
+
+#include "xdr/block.h"
+
 
 namespace speedex {
 
 std::unique_ptr<AccountModificationBlock>
-edce_persist_critical_round_data(
-	EdceManagementStructures& management_structures,
+persist_critical_round_data(
+	SpeedexManagementStructures& management_structures,
 	const HashedBlock& header,
 	BlockDataPersistenceMeasurements& measurements,
 	bool get_block = false,
 	uint64_t log_offset = 0);
 
 void 
-edce_persist_async(
-	EdceManagementStructures& management_structures,
+persist_async_phase1(
+	SpeedexManagementStructures& management_structures,
 	const uint64_t current_block_number,
 	BlockDataPersistenceMeasurements& measurements);
 
-
 void
-edce_persist_async_phase2(
-	EdceManagementStructures& management_structures,
+persist_async_phase2(
+	SpeedexManagementStructures& management_structures,
 	uint64_t current_block_number,
 	BlockDataPersistenceMeasurements& measurements);
 
 void
-edce_persist_async_phase3(
-	EdceManagementStructures& management_structures,
+persist_async_phase3(
+	SpeedexManagementStructures& management_structures,
 	uint64_t current_block_number,
 	BlockDataPersistenceMeasurements& measurements);
 
-struct EdceAsyncPersisterPhase3 : public AsyncWorker {
+class AsyncPersisterPhase3 : public AsyncWorker {
 	using AsyncWorker::mtx;
 	using AsyncWorker::cv;
 
-	EdceManagementStructures& management_structures;
+	SpeedexManagementStructures& management_structures;
 	BlockDataPersistenceMeasurements* latest_measurements = nullptr;
 	std::optional<uint64_t> current_block_number = std::nullopt;
 	
@@ -42,36 +62,21 @@ struct EdceAsyncPersisterPhase3 : public AsyncWorker {
 		return latest_measurements != nullptr;
 	}
 
+	void run();
 
-	void run() {
-		while(true) {
-			std::unique_lock lock(mtx);
-			if ((!done_flag) && (!exists_work_to_do())) {
-				cv.wait(lock, [this] () { return done_flag || exists_work_to_do();});
-			}
-			if (done_flag) return;
-			if (latest_measurements == nullptr) {
-				throw std::runtime_error("invalid call to async_persist_phase3!");
-			}
-			edce_persist_async_phase3(management_structures, *current_block_number, *latest_measurements);
-			latest_measurements = nullptr;
-			current_block_number = std::nullopt;
-			cv.notify_all();
-		}
-	}
+public:
 
-	EdceAsyncPersisterPhase3(EdceManagementStructures& management_structures)
+	AsyncPersisterPhase3(SpeedexManagementStructures& management_structures)
 		: AsyncWorker()
 		, management_structures(management_structures) {
 			start_async_thread([this] {run();});
 		}
 
-	void wait_for_async_persist_phase3() {
+	void do_async_persist_phase3(
+		uint64_t current_block_number_caller, 
+		BlockDataPersistenceMeasurements* measurements) 
+	{
 		wait_for_async_task();
-	}
-
-	void do_async_persist_phase3(uint64_t current_block_number_caller, BlockDataPersistenceMeasurements* measurements) {
-		wait_for_async_persist_phase3();
 
 		std::lock_guard lock(mtx);
 		latest_measurements = measurements;
@@ -79,63 +84,46 @@ struct EdceAsyncPersisterPhase3 : public AsyncWorker {
 		cv.notify_one();
 	}
 
-	~EdceAsyncPersisterPhase3() {
-		wait_for_async_persist_phase3();
+	~AsyncPersisterPhase3() {
+		wait_for_async_task();
 		end_async_thread();
 	}
 };
 
 
-struct EdceAsyncPersisterPhase2 : public AsyncWorker {
+class AsyncPersisterPhase2 : public AsyncWorker {
 	using AsyncWorker::mtx;
 	using AsyncWorker::cv;
 
-	EdceManagementStructures& management_structures;
+	SpeedexManagementStructures& management_structures;
 	BlockDataPersistenceMeasurements* latest_measurements = nullptr;
 	std::optional<uint64_t> current_block_number = std::nullopt;
 
-
-	EdceAsyncPersisterPhase3 phase3_persist;
+	AsyncPersisterPhase3 phase3_persist;
 
 	bool exists_work_to_do() override final {
 		return latest_measurements != nullptr;
 	}
 
-	void run() {
-		while(true) {
-			std::unique_lock lock(mtx);
-			if ((!done_flag) && (!exists_work_to_do())) {
-				cv.wait(lock, [this] () { return done_flag || exists_work_to_do();});
-			}
-			if (done_flag) return;
-			if (latest_measurements == nullptr) {
-				throw std::runtime_error("invalid call to async_persist_phase2!");
-			}
+	void run();
 
-			edce_persist_async_phase2(management_structures, *current_block_number, *latest_measurements);
-			
-			phase3_persist.do_async_persist_phase3(*current_block_number, latest_measurements);
+	friend class AsyncPersister;
 
-			latest_measurements = nullptr;
-			current_block_number = std::nullopt;
-			cv.notify_all();
-		}
-	}
+public:
 
-	EdceAsyncPersisterPhase2(EdceManagementStructures& management_structures)
+	AsyncPersisterPhase2(SpeedexManagementStructures& management_structures)
 		: AsyncWorker()
 		, management_structures(management_structures)
 		, phase3_persist(management_structures)
-       		, syncer(management_structures)	{
+		{
 			start_async_thread([this] {run();});
 		}
 
-	void wait_for_async_persist_phase2() {
+	void do_async_persist_phase2(
+		uint64_t current_block_number_caller, 
+		BlockDataPersistenceMeasurements* measurements)\
+	{
 		wait_for_async_task();
-	}
-
-	void do_async_persist_phase2(uint64_t current_block_number_caller, BlockDataPersistenceMeasurements* measurements) {
-		wait_for_async_persist_phase2();
 
 		std::lock_guard lock(mtx);
 		latest_measurements = measurements;
@@ -143,16 +131,15 @@ struct EdceAsyncPersisterPhase2 : public AsyncWorker {
 		cv.notify_one();
 	}
 
-	~EdceAsyncPersisterPhase2() {
-		syncer.stop();
-		wait_for_async_persist_phase2();
+	~AsyncPersisterPhase2() {
+		wait_for_async_task();
 		end_async_thread();
 	}
 };
 
-struct EdceAsyncPersister {
-	std::mutex mtx;
-	std::condition_variable cv;
+struct AsyncPersister : public AsyncWorker {
+	using AsyncWorker::mtx;
+	using AsyncWorker::cv;
 
 	bool done_flag = false;
 
@@ -160,54 +147,49 @@ struct EdceAsyncPersister {
 
 	BlockDataPersistenceMeasurements* latest_measurements;
 
-	EdceManagementStructures& management_structures;
+	SpeedexManagementStructures& management_structures;
 
 	uint64_t highest_persisted_block;
 
-	EdceAsyncPersisterPhase2 phase2_persist;
+	AsyncPersisterPhase2 phase2_persist;
 
-	EdceAsyncPersister(EdceManagementStructures& management_structures)
-		: management_structures(management_structures)
-		, phase2_persist(management_structures) {
-			start_persist_thread();
-		}
-
-
-	void run() {
-		while (true) {
-			std::unique_lock lock(mtx);
-			if ((!block_number_to_persist) && (!done_flag)) {
-				cv.wait(lock, [this] { return ((bool) block_number_to_persist) || done_flag;});
-			}
-			if (done_flag) return;
-			edce_persist_async(management_structures, *block_number_to_persist, *latest_measurements);
-
-			phase2_persist.do_async_persist_phase2(*block_number_to_persist, latest_measurements);
-
-			highest_persisted_block = *block_number_to_persist;
-
-			block_number_to_persist = std::nullopt;
-			latest_measurements = nullptr;
-			cv.notify_one();
-		}
+	bool exists_work_to_do() override final {
+		return latest_measurements != nullptr;
 	}
+
+
+	void run();
+
+public:
+
+	AsyncPersister(SpeedexManagementStructures& management_structures)
+		: AsyncWorker()
+		, management_structures(management_structures)
+		, phase2_persist(management_structures) {
+			start_async_thread([this] {run();});
+		}
+
+
 
 	uint64_t get_highest_persisted_block() {
 		std::lock_guard lock(mtx);
 		return highest_persisted_block;
 	}
 
-	void do_async_persist(const uint64_t persist_block_number, BlockDataPersistenceMeasurements& measurements) {
+	void do_async_persist(
+		const uint64_t persist_block_number, 
+		BlockDataPersistenceMeasurements& measurements) {
 		
 		auto timestamp = init_time_measurement();
 
-		wait_for_async_persist_local();
+		wait_for_async_task();
 
 		measurements.wait_for_persist_time = measure_time(timestamp);
 		{
 			std::lock_guard lock(mtx);
 			if (block_number_to_persist) {
-				throw std::runtime_error("can't start persist before last one finishes!");
+				throw std::runtime_error(
+					"can't start persist before last one finishes!");
 			}
 			block_number_to_persist = persist_block_number;
 			latest_measurements = &measurements;
@@ -215,34 +197,16 @@ struct EdceAsyncPersister {
 		cv.notify_one();
 	}
 
-	void wait_for_async_persist_local() {
-		std::unique_lock lock(mtx);
-		if (!block_number_to_persist) return;
-		cv.wait(lock, [this] { return !block_number_to_persist; });
-	}
-
 	void wait_for_async_persist() {
 		//clears up all uses of measurements reference
-		wait_for_async_persist_local();
-		phase2_persist.wait_for_async_persist_phase2();
-		phase2_persist.phase3_persist.wait_for_async_persist_phase3();
+		wait_for_async_task();
+		phase2_persist.wait_for_async_task();
+		phase2_persist.phase3_persist.wait_for_async_task();
 	}
 
-	void start_persist_thread() {
-		std::thread([this] () {
-			run();
-		}).detach();
-	}
-
-	void end_persist_thread() {
-		std::lock_guard lock(mtx);
-		done_flag = true;
-		cv.notify_one();
-	}
-
-	~EdceAsyncPersister() {
+	~AsyncPersister() {
 		wait_for_async_persist();
-		end_persist_thread();
+		end_async_thread();
 	}
 };
 
