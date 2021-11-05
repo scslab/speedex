cf6993c21eadcdb3042d2769a5655a2b9a564e74
gramseyer <ramseyer@uchicago.edu>
2021-11-04 18:43:38 -0700

    update synthetic_data_gen to support sharded account output

--- main/synthetic_data_gen_from_params.cc
+++ main/synthetic_data_gen_from_params.cc
@@ -1,5 +1,7 @@
 #include <cstddef>
 
+#include "config/replica_config.h"
+
 #include "speedex/speedex_options.h"
 
 #include "synthetic_data_generator/synthetic_data_gen.h"
@@ -7,38 +9,143 @@
 
 #include "xdr/experiments.h"
 
+#include <getopt.h>
+#include <libfyaml.h>
+
 using namespace speedex;
 
-int main(int argc, char const *argv[])
+[[noreturn]]
+static void usage() {
+	std::printf(R"(
+usage: synthetic_data_gen --speedex_options=<options_yaml, required> 
+                          --exp_options=<experiment_options_yaml, required>
+                          --exp_name=<experiment_name, required>
+                          --just_params (optional)
+                          --replica_id=<id, optional> 
+                          --config_file=<filename, optional>
+)");
+	exit(1);
+}
+
+enum opttag {
+	OPT_REPLICA_ID = 0x100,
+	OPT_CONFIG_FILE,
+	SPEEDEX_OPTIONS,
+	EXPERIMENT_OPTIONS,
+	EXPERIMENT_NAME,
+	JUST_PARAMS
+};
+
+static const struct option opts[] = {
+	{"replica_id", required_argument, nullptr, OPT_REPLICA_ID},
+	{"config_file", required_argument, nullptr, OPT_CONFIG_FILE},
+	{"speedex_options", required_argument, nullptr, SPEEDEX_OPTIONS},
+	{"exp_options", required_argument, nullptr, EXPERIMENT_OPTIONS},
+	{"exp_name", required_argument, nullptr, EXPERIMENT_NAME},
+	{"just_params", no_argument, nullptr, JUST_PARAMS},
+	{nullptr, 0, nullptr, 0}
+};
+
+
+
+int main(int argc, char* const* argv)
 {
-	if (!(argc == 4 || argc == 5)) {
-		std::printf("usage: ./synthetic_data_gen <speedex_options_yaml> <experiment_yaml> <experiment_name> <just_params>\n");
-		return 1;
+	//if (!(argc == 4 || argc == 5)) {
+	//	std::printf("usage: ./synthetic_data_gen <speedex_options_yaml> <experiment_yaml> <experiment_name> <just_params>\n");
+	//	return 1;
+	//}
+
+	std::optional<ReplicaID> self_id;
+	std::optional<std::string> config_file;
+
+	bool just_params = false;
+
+	std::string speedex_options_file;
+	std::string experiment_options_file;
+	std::string experiment_name;
+	
+	int opt;
+
+	while ((opt = getopt_long_only(argc, argv, "",
+				 opts, nullptr)) != -1)
+	{
+		switch(opt) {
+			case OPT_REPLICA_ID:
+				self_id = std::stol(optarg);
+				break;
+			case OPT_CONFIG_FILE:
+				config_file = optarg;
+				break;
+			case SPEEDEX_OPTIONS:
+				speedex_options_file = optarg;
+				break;
+			case EXPERIMENT_OPTIONS:
+				experiment_options_file = optarg;
+				break;
+			case EXPERIMENT_NAME:
+				experiment_name = optarg;
+				break;
+			case JUST_PARAMS:
+				just_params = true;
+				break;
+			default:
+				usage();
+		}
 	}
+
+	if (config_file.has_value() != self_id.has_value()) {
+		usage();
+	}
+
+	std::optional<std::pair<ReplicaID, ReplicaConfig>> conf_pair;
+	if (config_file.has_value()) {
+		conf_pair = std::make_optional<std::pair<ReplicaID, ReplicaConfig>>();
+		conf_pair->first = *self_id;
+
+		struct fy_document* fyd = fy_document_build_from_file(NULL, config_file->c_str());
+		if (fyd == NULL) {
+			std::printf("Failed to build doc from file \"%s\"\n", config_file->c_str());
+			usage();
+		}
+
+		conf_pair->second.parse(fyd, *self_id);
+	}
+
+	if (speedex_options_file.size() == 0) {
+		usage();
+	}
+
+	if (experiment_options_file.size() == 0) {
+		usage();
+	}
+
+	if (experiment_name.size() == 0) {
+		usage();
+	}
+
 
 	std::minstd_rand gen(0);
 
 	GenerationOptions options;
-	auto parsed = options.parse(argv[2]);
+	auto parsed = options.parse(experiment_options_file.c_str());
 	if (!parsed) {
-		std::printf("yaml parse error\n");
+		std::printf("failed to parse experiment options file\n");
 		return 1;
 	}
-	std::printf("done parse\n");
 
 	ExperimentParameters params;
 	params.num_assets = options.num_assets;
 	params.num_accounts = options.num_accounts;
 
 	SpeedexOptions speedex_options;
-	speedex_options.parse_options(argv[1]);
+	speedex_options.parse_options(speedex_options_file.c_str());
 	params.tax_rate = speedex_options.tax_rate;
 	params.smooth_mult = speedex_options.smooth_mult;
 	params.num_threads = 0; // SET LATER
 	params.persistence_frequency = speedex_options.persistence_frequency;
 	params.num_blocks = options.num_blocks;
 
-	std::string output_root = options.output_prefix + std::string(argv[3]) + std::string("/");
+	std::string output_root = options.output_prefix + experiment_name + std::string("/");
 
 	if (mkdir_safe(options.output_prefix.c_str())) {
 		std::printf("directory %s already exists, continuing\n", options.output_prefix.c_str());
@@ -56,9 +163,9 @@ int main(int argc, char const *argv[])
 		throw std::runtime_error("failed to save params file");
 	}
 
-	GeneratorState generator (gen, options, output_root);
+	GeneratorState generator (gen, options, output_root, conf_pair);
 
-	if (argc == 4) {
+	if (!just_params) {
 		generator.make_blocks();
 	}
 
