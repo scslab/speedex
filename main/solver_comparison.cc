93ab2aa01393964cc1d9226d3bc3bc299f848f24
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-03-03 10:40:28 -0800

    lemon solver integration

--- main/solver_comparison.cc
+++ main/solver_comparison.cc
@@ -13,7 +13,18 @@
 #include <cstdint>
 #include <memory>
 #include <random>
+#include <utility>
 #include <vector>
+
+#include "config.h"
+
+#if HAVE_LEMON == 1
+
+#include <lemon/list_graph.h>
+#include <lemon/network_simplex.h>
+#include <lemon/maps.h>
+
+#endif
 
 using namespace speedex;
 
@@ -106,11 +117,96 @@ bool run_simplex(std::vector<BoundsInfo> const& info, std::vector<Price> const& 
 	}*/
 }
 
+#if HAVE_LEMON == 1
+
+using lemon_instance_t 
+	= std::tuple<
+		std::unique_ptr<lemon::ListDigraph>,
+		std::unique_ptr<lemon::NetworkSimplex<lemon::ListDigraph>>, 
+		std::unique_ptr<std::map<std::pair<size_t, size_t>, lemon::ListDigraph::Arc>>
+	>;
+
+lemon_instance_t
+make_lemon_instance(size_t num_assets)
+{
+	std::unique_ptr<lemon::ListDigraph> d = std::make_unique<lemon::ListDigraph>();
+
+	std::vector<lemon::ListDigraph::Node> nodes;
+
+	auto arcs = std::make_unique<std::map<std::pair<size_t, size_t>, lemon::ListDigraph::Arc>>();
+
+	for (size_t i = 0; i < num_assets; i ++) {
+		nodes.push_back(d->addNode());
+	}
+
+	for (size_t i = 0; i < num_assets; i++) {
+		for (size_t j = 0; j < num_assets; j++) {
+			if (i != j) {
+				auto arc = d->addArc(nodes[i], nodes[j]);
+
+				auto pair = std::make_pair(i, j);
+				arcs->emplace(pair, arc);
+			}
+		}
+	}
+
+	auto ns = std::make_unique<lemon::NetworkSimplex<lemon::ListDigraph>>(*d);
+
+	return std::make_tuple(std::move(d), std::move(ns), std::move(arcs));
+}
+
+#else
+
+using lemon_instance_t = int;
+
+#endif
+
+
+#if HAVE_LEMON == 1
+
+bool run_lemon_ns(std::vector<BoundsInfo> const& info, std::vector<Price> const& prices, size_t num_assets, lemon_instance_t& instance) {
+
+	auto& d = *std::get<0>(instance);
+	auto& ns = *std::get<1>(instance);
+	auto& arcs = *std::get<2>(instance);
+
+	lemon::ListDigraph::ArcMap<int128_t> ubs(d), lbs(d), costs(d);
+
+	for (auto const& b : info) {
+		auto& arc = arcs[{b.category.sellAsset, b.category.buyAsset}];
+		int128_t lb = ((int128_t)b.bounds.first) * ((int128_t) prices[b.category.sellAsset]);
+		int128_t ub = ((int128_t)b.bounds.second) * ((int128_t) prices[b.category.sellAsset]);
+
+		ubs.set(arc, ub);
+		lbs.set(arc, lb);
+		costs.set(arc, -1);
+	}
+
+	ns.reset();
+	ns.upperMap(ubs).lowerMap(lbs).costMap(costs);
+
+	auto res = ns.run();
+
+	using res_t = lemon::NetworkSimplex<lemon::ListDigraph>::ProblemType;
+
+	if (res == res_t::UNBOUNDED) {
+		throw std::runtime_error("mistaken setup");
+	}
+
+	//std::printf("total lemon value = %ld\n", -ns.totalCost());
+
+	return res == res_t::OPTIMAL;
+}
+
+#endif
+
 float overall_sum_simplex = 0;
 size_t count = 0;
 
+bool can_run_simplex = true;
+
 template<typename rand_gen>
-void run_experiment(size_t num_assets, std::unique_ptr<LPInstance>& instance, LPSolver& lp_solver, rand_gen& gen, size_t& glpk_successes, size_t& simplex_successes) {
+void run_experiment(size_t num_assets, std::unique_ptr<LPInstance>& instance, LPSolver& lp_solver, lemon_instance_t& lemon_instance, rand_gen& gen, size_t& glpk_successes, size_t& simplex_successes, size_t& lemon_successes) {
 
 	auto prices = gen_prices(num_assets, gen);
 	auto bounds = gen_bounds(num_assets, gen);
@@ -122,12 +218,14 @@ void run_experiment(size_t num_assets, std::unique_ptr<LPInstance>& instance, LP
 	if (run_glpk(bounds, prices, instance, lp_solver, num_assets)) {
 		glpk_successes++;
 		glpk_res = true;
-	}
+	} 
 
 	float glpk_time = measure_time(ts);
 
-	if(run_simplex(bounds, prices, num_assets, glpk_res)) {
-		simplex_successes++;
+	if (can_run_simplex) {
+		if(run_simplex(bounds, prices, num_assets, glpk_res)) {
+			simplex_successes++;
+		}
 	}
 
 	float simplex_time = measure_time(ts);
@@ -135,8 +233,19 @@ void run_experiment(size_t num_assets, std::unique_ptr<LPInstance>& instance, LP
 	overall_sum_simplex += simplex_time;
 	count++;
 
-	std::printf("glpk_time(successes=%lu) %lf\t simplex_time(successes=%lu) %lf (avg %lf)\n",
-		glpk_successes, glpk_time, simplex_successes, simplex_time, overall_sum_simplex / count);
+	#if HAVE_LEMON == 1
+
+	if(run_lemon_ns(bounds, prices, num_assets, lemon_instance))
+	{
+		lemon_successes ++;
+	}
+
+	#endif
+
+	float lemon_time = measure_time(ts);
+
+	std::printf("glpk_time(successes=%lu) %lf\t simplex_time(successes=%lu) %lf (avg %lf) lemon-ns(successes=%lu) %lf\n",
+		glpk_successes, glpk_time, simplex_successes, simplex_time, overall_sum_simplex / count, lemon_successes, lemon_time);
 }
 
 
@@ -151,20 +260,32 @@ int main(int argc, char const *argv[])
 
 	if ((2* num_assets * num_assets + 2 * num_assets) > ((size_t) UINT16_MAX)) {
 		std::printf("too many assets, will overflow inside simplex solver\n");
-		return 1;
-	}
+		can_run_simplex = false;
+	} 
 
 	auto instance = make_instance(num_assets);
 	OrderbookManager manager(num_assets);
 	LPSolver lp_solver(manager);
 
+	#if HAVE_LEMON == 1
+
+	auto lemon_inst = make_lemon_instance(num_assets);
+
+	#else
+
+	lemon_instance_t lemon_inst = 0;
+	std::printf("lemon library not found, skipping those solvers\n");
+
+	#endif
+
 	size_t glpk_successes = 0;
 	size_t simplex_successes = 0;
+	size_t lemon_successes = 0;
 
 	std::minstd_rand gen(0);
 
 	while(true) {
-		run_experiment(num_assets, instance, lp_solver, gen, glpk_successes, simplex_successes);
+		run_experiment(num_assets, instance, lp_solver, lemon_inst, gen, glpk_successes, simplex_successes, lemon_successes);
 	}
 }
 
