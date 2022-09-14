a752486433131e853b20f9120287dc47b411cc29
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-09-13 19:19:29 -0700

    add mtx to header_hash (in case there's accidentally a persist event concurrent with a hotstuff-initiated rollback, and a future change removes a higher-level locking mechanism on header_hash)

--- header_hash/block_header_hash_map.cc
+++ header_hash/block_header_hash_map.cc
@@ -6,168 +6,213 @@
 #include "utils/debug_macros.h"
 #include "utils/debug_utils.h"
 
-namespace speedex {
+namespace speedex
+{
 
 using xdr::operator==;
 
 using key_byte_array_t = std::array<uint8_t, BlockHeaderHashMap::KEY_LEN>;
 
-void 
-BlockHeaderHashMapLMDB::open_env() {
-	LMDBInstance::open_env(
-		std::string(ROOT_DB_DIRECTORY) + std::string(HEADER_HASH_DB),
-		DEFAULT_LMDB_FLAGS | MDB_NOLOCK); // need NOLOCK for commit/reload tests
+void
+BlockHeaderHashMapLMDB::open_env()
+{
+    LMDBInstance::open_env(
+        std::string(ROOT_DB_DIRECTORY) + std::string(HEADER_HASH_DB),
+        DEFAULT_LMDB_FLAGS | MDB_NOLOCK); // need NOLOCK for commit/reload tests
 }
 
-void BlockHeaderHashMap::insert(const Block& block, bool res) {
+void
+BlockHeaderHashMap::insert(const Block& block, bool res)
+{
 
-	uint64_t block_number = block.blockNumber;
-	if (block_number == 0) {
-		throw std::runtime_error("should never insert genesis hash!");
-	}
+    std::lock_guard lock(mtx);
 
-	// block header hash map requires strict sequentiality, unlike memdb thunks & orderbook thunks
-	if (block_number != last_committed_block_number + 1) {
-		throw std::runtime_error("inserting wrong block number");
-	}
+    uint64_t block_number = block.blockNumber;
+    if (block_number == 0)
+    {
+        throw std::runtime_error("should never insert genesis hash!");
+    }
 
-	prefix_t key_buf;
+    // block header hash map requires strict sequentiality, unlike memdb thunks
+    // & orderbook thunks
+    if (block_number != last_committed_block_number + 1)
+    {
+        throw std::runtime_error("inserting wrong block number");
+    }
 
-	utils::write_unsigned_big_endian(key_buf, block_number);
+    prefix_t key_buf;
 
-	BlockHeaderHashValue value;
-	value.hash = hash_xdr(block);
-	value.validation_success = res ? 1 : 0;
+    utils::write_unsigned_big_endian(key_buf, block_number);
 
-	block_map.insert(key_buf, ValueT(value));
-	
-	last_committed_block_number = block_number;
+    BlockHeaderHashValue value;
+    value.hash = hash_xdr(block);
+    value.validation_success = res ? 1 : 0;
+
+    block_map.insert(key_buf, ValueT(value));
+
+    last_committed_block_number = block_number;
 }
 
-void BlockHeaderHashMap::persist_lmdb(uint64_t current_block_number) {
-	BLOCK_INFO("persisting header hash map at round %lu", current_block_number);
+void
+BlockHeaderHashMap::persist_lmdb(uint64_t current_block_number)
+{
 
-	if (!lmdb_instance) {
-		return;
-	}
-	uint64_t persisted_round_number 
-		= lmdb_instance.get_persisted_round_number();
+    std::lock_guard lock(mtx);
 
-	auto wtx = lmdb_instance.wbegin();
+    BLOCK_INFO("persisting header hash map at round %lu", current_block_number);
 
-	//changed: we commit current_block_number because we've already inserted it.
-	for (uint64_t i = persisted_round_number; i <= current_block_number; i++) { 
-		if (i == 0) continue;
-		TrieT::prefix_t round_buf;
+    if (!lmdb_instance)
+    {
+        return;
+    }
+    uint64_t persisted_round_number
+        = lmdb_instance.get_persisted_round_number();
 
-		utils::write_unsigned_big_endian(round_buf, i);
+    auto wtx = lmdb_instance.wbegin();
 
-		auto round_bytes = round_buf.template get_bytes_array<key_byte_array_t>();
-		dbval key{round_bytes};
+    // changed: we commit current_block_number because we've already inserted
+    // it.
+    for (uint64_t i = persisted_round_number; i <= current_block_number; i++)
+    {
+        if (i == 0)
+            continue;
+        TrieT::prefix_t round_buf;
 
-		// querying for round i
-		auto hash_opt = block_map.get_value(round_buf);
-		if (!hash_opt) {
-			throw std::runtime_error("did not find hash in hash_map!");
-		}
+        utils::write_unsigned_big_endian(round_buf, i);
 
-		BlockHeaderHashValue unwrapped_value = *hash_opt;
+        auto round_bytes
+            = round_buf.template get_bytes_array<key_byte_array_t>();
+        dbval key{ round_bytes };
 
-		auto cur_bytes = xdr::xdr_to_opaque(unwrapped_value);
+        // querying for round i
+        auto hash_opt = block_map.get_value(round_buf);
+        if (!hash_opt)
+        {
+            throw std::runtime_error("did not find hash in hash_map!");
+        }
 
-		dbval hash_val{cur_bytes};
-		wtx.put(lmdb_instance.get_data_dbi(), &key, &hash_val);
-	}
+        BlockHeaderHashValue unwrapped_value = *hash_opt;
 
-	lmdb_instance.commit_wtxn(wtx, current_block_number);
+        auto cur_bytes = xdr::xdr_to_opaque(unwrapped_value);
+
+        dbval hash_val{ cur_bytes };
+        wtx.put(lmdb_instance.get_data_dbi(), &key, &hash_val);
+    }
+
+    lmdb_instance.commit_wtxn(wtx, current_block_number);
 }
 
 // LMDB committed to round X contains entries 1 through X.
 // To sync back with LMDB, we need to remove all entries X+1 and higher.
 
-void 
+void
 BlockHeaderHashMap::rollback_to_committed_round(uint64_t committed_block_number)
 {
-	if (committed_block_number < lmdb_instance.get_persisted_round_number()) {
-		throw std::runtime_error("can't rollback beyond lmdb persist");
-	}
-	for (uint64_t i = committed_block_number + 1; i <= last_committed_block_number; i++) {
-		if (i == 0) continue;
-		TrieT::prefix_t round_buf;
-		utils::write_unsigned_big_endian(round_buf, i);
+    std::lock_guard lock(mtx);
 
-		if (!block_map.perform_deletion(round_buf)) {
-			throw std::runtime_error("error when deleting from header hash map");
-		}
-	}
-	last_committed_block_number = committed_block_number;//(committed_block_number == 0) ? 0 : committed_block_number - 1;
+    if (committed_block_number < lmdb_instance.get_persisted_round_number())
+    {
+        throw std::runtime_error("can't rollback beyond lmdb persist");
+    }
+    for (uint64_t i = committed_block_number + 1;
+         i <= last_committed_block_number;
+         i++)
+    {
+        if (i == 0)
+            continue;
+        TrieT::prefix_t round_buf;
+        utils::write_unsigned_big_endian(round_buf, i);
+
+        if (!block_map.perform_deletion(round_buf))
+        {
+            throw std::runtime_error(
+                "error when deleting from header hash map");
+        }
+    }
+    last_committed_block_number
+        = committed_block_number; //(committed_block_number == 0) ? 0 :
+                                  //committed_block_number - 1;
 }
 
-void BlockHeaderHashMap::load_lmdb_contents_to_memory() {
-	auto rtx = lmdb_instance.rbegin();
+void
+BlockHeaderHashMap::load_lmdb_contents_to_memory()
+{
 
-	auto cursor = rtx.cursor_open(lmdb_instance.get_data_dbi());
+    std::lock_guard lock(mtx);
 
-	for (auto kv : cursor) {
-		auto bytes = kv.first.bytes();
+    auto rtx = lmdb_instance.rbegin();
 
-		TrieT::prefix_t prefix;
-		prefix.from_bytes_array(bytes);
+    auto cursor = rtx.cursor_open(lmdb_instance.get_data_dbi());
 
-		uint64_t round_number;
+    for (auto kv : cursor)
+    {
+        auto bytes = kv.first.bytes();
 
-		utils::read_unsigned_big_endian(prefix, round_number);
+        TrieT::prefix_t prefix;
+        prefix.from_bytes_array(bytes);
 
-		if (round_number > lmdb_instance.get_persisted_round_number()) {
+        uint64_t round_number;
 
-			std::printf(
-				"round number: %" PRIu64 " persisted_round_number: %" PRIu64 "\n", 
-				round_number, 
-				lmdb_instance.get_persisted_round_number());
-			std::fflush(stdout);
-			throw std::runtime_error(
-				"lmdb contains round idx beyond committed max");
-		}
+        utils::read_unsigned_big_endian(prefix, round_number);
 
-		prefix_t round_buf;
-		utils::write_unsigned_big_endian(round_buf, round_number);
+        if (round_number > lmdb_instance.get_persisted_round_number())
+        {
 
-		BlockHeaderHashValue block;
-		//if (kv.second.mv_size != 32) {
-		//	throw std::runtime_error("invalid value size");
-		//}
+            std::printf("round number: %" PRIu64
+                        " persisted_round_number: %" PRIu64 "\n",
+                        round_number,
+                        lmdb_instance.get_persisted_round_number());
+            std::fflush(stdout);
+            throw std::runtime_error(
+                "lmdb contains round idx beyond committed max");
+        }
 
-		auto db_value_bytes = kv.second.bytes();
-		xdr::xdr_from_opaque(db_value_bytes, block);
+        prefix_t round_buf;
+        utils::write_unsigned_big_endian(round_buf, round_number);
 
-		//memcpy(value.data(), kv.second.mv_data, 32);
-		block_map.insert(round_buf, ValueT(block));
-	}
-	last_committed_block_number = lmdb_instance.get_persisted_round_number();
-	rtx.commit();
+        BlockHeaderHashValue block;
+        // if (kv.second.mv_size != 32) {
+        //	throw std::runtime_error("invalid value size");
+        // }
+
+        auto db_value_bytes = kv.second.bytes();
+        xdr::xdr_from_opaque(db_value_bytes, block);
+
+        // memcpy(value.data(), kv.second.mv_data, 32);
+        block_map.insert(round_buf, ValueT(block));
+    }
+    last_committed_block_number = lmdb_instance.get_persisted_round_number();
+    rtx.commit();
 }
 
 std::optional<BlockHeaderHashValue>
-BlockHeaderHashMap::get(uint64_t round_number) const {
-	if (round_number > get_persisted_round_number()) {
-		return std::nullopt;
-	}
+BlockHeaderHashMap::get(uint64_t round_number) const
+{
 
-	prefix_t round_buf;
-	utils::write_unsigned_big_endian(round_buf, round_number);
-	auto* out_opt = block_map.get_value(round_buf);
+    std::lock_guard lock(mtx);
 
-	if (!out_opt) {
-		throw std::runtime_error("failed to load hash that lmdb should have");
-	}
+    if (round_number > get_persisted_round_number())
+    {
+        return std::nullopt;
+    }
 
-	return {*out_opt};
+    prefix_t round_buf;
+    utils::write_unsigned_big_endian(round_buf, round_number);
+    auto* out_opt = block_map.get_value(round_buf);
+
+    if (!out_opt)
+    {
+        throw std::runtime_error("failed to load hash that lmdb should have");
+    }
+
+    return { *out_opt };
 }
 
-void 
-LoadLMDBHeaderMap::insert_for_loading(Block const& block, bool validation_success) {
-	return generic_do<&BlockHeaderHashMap::insert>(
-		block, validation_success);
+void
+LoadLMDBHeaderMap::insert_for_loading(Block const& block,
+                                      bool validation_success)
+{
+    return generic_do<&BlockHeaderHashMap::insert>(block, validation_success);
 }
 
-} /* speedex */
+} // namespace speedex
--- header_hash/block_header_hash_map.h
+++ header_hash/block_header_hash_map.h
@@ -5,7 +5,7 @@ The block header-hash map is a merkle trie mapping block number to block hash.
 
 Possible future optimization: Block numbers increment sequentially.
 Once some subtrie fills up, it will never be modified again.  We don't need
-to load that data into memory.  Would only be relevant if Speedex runs for 
+to load that data into memory.  Would only be relevant if Speedex runs for
 millions of blocks.
 */
 
@@ -17,118 +17,117 @@ millions of blocks.
 #include "mtt/trie/metadata.h"
 #include "mtt/trie/prefix.h"
 
-#include "xdr/types.h"
 #include "xdr/block.h"
+#include "xdr/types.h"
 
 #include <cstdint>
 #include <map>
 #include <optional>
 
-namespace speedex {
+namespace speedex
+{
 
 // After commitment of block N, contains hashes of rounds 1 to N, *inclusive*
 
 /*! LMDB instance for persisting block header hashes to disk
-*/
-struct BlockHeaderHashMapLMDB : public LMDBInstance {
-	constexpr static auto DB_NAME = "header_hash_lmdb";
+ */
+struct BlockHeaderHashMapLMDB : public LMDBInstance
+{
+    constexpr static auto DB_NAME = "header_hash_lmdb";
 
-	BlockHeaderHashMapLMDB() : LMDBInstance() {}
+    BlockHeaderHashMapLMDB()
+        : LMDBInstance()
+    {}
 
-	void open_env();
+    void open_env();
 
-	void create_db() {
-		LMDBInstance::create_db(DB_NAME);
-	}
+    void create_db() { LMDBInstance::create_db(DB_NAME); }
 
-	void open_db() {
-		LMDBInstance::open_db(DB_NAME);
-	}
+    void open_db() { LMDBInstance::open_db(DB_NAME); }
 };
 
 /*! Stores a merkle trie mapping block numbers to block root hashes.
-*/
-class BlockHeaderHashMap {
+ */
+class BlockHeaderHashMap
+{
 
-	static std::vector<uint8_t> 
-	serialize(const BlockHeaderHashValue& v)
-	{
-		return xdr::xdr_to_opaque(v);
-	}
+    static std::vector<uint8_t> serialize(const BlockHeaderHashValue& v)
+    {
+        return xdr::xdr_to_opaque(v);
+    }
 
-	using ValueT = trie::XdrTypeWrapper<BlockHeaderHashValue, &serialize>;
+    using ValueT = trie::XdrTypeWrapper<BlockHeaderHashValue, &serialize>;
 
-	using prefix_t = trie::UInt64Prefix;
+    using prefix_t = trie::UInt64Prefix;
 
-	using MetadataT = trie::CombinedMetadata<trie::SizeMixin>;
+    using MetadataT = trie::CombinedMetadata<trie::SizeMixin>;
 
-	using TrieT = trie::MerkleTrie<prefix_t, ValueT, MetadataT>;
+    using TrieT = trie::MerkleTrie<prefix_t, ValueT, MetadataT>;
 
-	TrieT block_map;
+    TrieT block_map;
 
-	BlockHeaderHashMapLMDB lmdb_instance;
+    BlockHeaderHashMapLMDB lmdb_instance;
 
-	uint64_t last_committed_block_number;
+    uint64_t last_committed_block_number;
+
+    std::mutex mtx;
 
 public:
-	
-	constexpr static unsigned int KEY_LEN = sizeof(uint64_t);
+    constexpr static unsigned int KEY_LEN = sizeof(uint64_t);
 
-	//! Construct empty map.
-	BlockHeaderHashMap() 
-		: block_map(), lmdb_instance(), last_committed_block_number(0) {}
+    //! Construct empty map.
+    BlockHeaderHashMap()
+        : block_map()
+        , lmdb_instance()
+        , last_committed_block_number(0)
+    {}
 
-	/*! Insert hash of a newly produced block.
-		In normal operation, map should include hashes for 
-		[0, last_committed_block_number) and block_number input is 
-		prev_block = last_committed_block_number
-	*/
-	void insert(const Block& block, bool validation_success);
+    /*! Insert hash of a newly produced block.
+            In normal operation, map should include hashes for
+            [0, last_committed_block_number) and block_number input is
+            prev_block = last_committed_block_number
+    */
+    void insert(const Block& block, bool validation_success);
 
-	//! Hash the merkle trie.
-	void hash(Hash& hash) {
-		block_map.hash(hash);
-	}
+    //! Hash the merkle trie.
+    void hash(Hash& hash) { block_map.hash(hash); }
 
-	void open_lmdb_env() {
-		lmdb_instance.open_env();
-	}
-	void create_lmdb() {
-		lmdb_instance.create_db();
-	}
-	void open_lmdb() {
-		lmdb_instance.open_db();
-	}
+    void open_lmdb_env() { lmdb_instance.open_env(); }
+    void create_lmdb() { lmdb_instance.create_db(); }
+    void open_lmdb() { lmdb_instance.open_db(); }
 
-	//! Persist block hashes to LMDB, up to current block number.
-	void persist_lmdb(uint64_t current_block_number);
+    //! Persist block hashes to LMDB, up to current block number.
+    void persist_lmdb(uint64_t current_block_number);
 
-	void rollback_to_committed_round(uint64_t committed_block_number);
+    void rollback_to_committed_round(uint64_t committed_block_number);
 
-	//! Get the block number reflected in disk state.
-	uint64_t get_persisted_round_number() const {
-		return lmdb_instance.get_persisted_round_number();
-	}
+    //! Get the block number reflected in disk state.
+    uint64_t get_persisted_round_number() const
+    {
+        return lmdb_instance.get_persisted_round_number();
+    }
 
-	//! Read in trie contents from disk.
-	void load_lmdb_contents_to_memory();
+    //! Read in trie contents from disk.
+    void load_lmdb_contents_to_memory();
 
-	std::optional<BlockHeaderHashValue> get(uint64_t round_number) const;
+    std::optional<BlockHeaderHashValue> get(uint64_t round_number) const;
 };
 
 //! Mock around BlockHeaderHashMap that makes calls into no-ops when replaying
 //! a block who's state changes are already reflected in lmdb.
-class LoadLMDBHeaderMap : public LMDBLoadingWrapper<BlockHeaderHashMap&> {
+class LoadLMDBHeaderMap : public LMDBLoadingWrapper<BlockHeaderHashMap&>
+{
 
-	using LMDBLoadingWrapper<BlockHeaderHashMap&> :: generic_do;
+    using LMDBLoadingWrapper<BlockHeaderHashMap&>::generic_do;
+
 public:
-	LoadLMDBHeaderMap(
-		uint64_t current_block_number,
-		BlockHeaderHashMap& main_db) 
-	: LMDBLoadingWrapper<BlockHeaderHashMap&>(current_block_number, main_db) {}
+    LoadLMDBHeaderMap(uint64_t current_block_number,
+                      BlockHeaderHashMap& main_db)
+        : LMDBLoadingWrapper<BlockHeaderHashMap&>(current_block_number, main_db)
+    {}
 
-	//! Insert a block hash when replaying trusted blocks.
-	void insert_for_loading(Block const& block, bool validation_success);
+    //! Insert a block hash when replaying trusted blocks.
+    void insert_for_loading(Block const& block, bool validation_success);
 };
 
-} /* speedex */
+} // namespace speedex
