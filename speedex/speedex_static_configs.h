4fcb52d8ea7ca4f906fe07c83fb3b71cdd49988f
Ubuntu <ubuntu@ip-172-31-34-180.us-east-2.compute.internal>
2022-10-11 00:08:06 +0000

    disable tat thread

--- speedex/speedex_static_configs.h
+++ speedex/speedex_static_configs.h
@@ -10,6 +10,12 @@ namespace speedex
   #define _MAX_SEQ_NUMS_PER_BLOCK 64
 #endif
 
+#ifndef DISABLE_TATONNEMENT_TIMEOUT
+	constexpr static bool USE_TATONNEMENT_TIMEOUT_THREAD = true;
+#else
+	constexpr static bool USE_TATONNEMENT_TIMEOUT_THREAD = false;
+#endif
+
 constexpr static uint32_t MAX_SEQ_NUMS_PER_BLOCK = _MAX_SEQ_NUMS_PER_BLOCK;
 
 constexpr static bool DETAILED_MOD_LOGGING = true;
