92e0d40205f288503f31f84bae95399bb00c6dec
Ubuntu <ubuntu@ip-172-31-23-31.us-east-2.compute.internal>
2022-10-17 01:21:09 +0000

    update scripts

---
+++ disable_hyperthreading.sh
@@ -0,0 +1,5 @@
+for cpunum in $(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | cut -s -d, -f2- | tr ',' '\n' | sort -un)
+do
+	echo $cpunum
+	echo 0 > /sys/devices/system/cpu/cpu$cpunum/online
+done
