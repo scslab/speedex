1468cb13272f95670f7b357a91f8007b63dab508
Geoff Ramseyer <gdwr@cs.stanford.edu>
2023-03-04 15:13:27 -0800

    copyright notices on each source file

--- cda/serial_ob_experiment.cc
+++ cda/serial_ob_experiment.cc
@@ -1,8 +1,25 @@
+/**
+ * SPEEDEX: A Scalable, Parallelizable, and Economically Efficient Decentralized Exchange
+ * Copyright (C) 2023 Geoffrey Ramseyer
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "cda/serial_ob_experiment.h"
 
 #include "memory_database/memory_database.h"
 #include "memory_database/memory_database_view.h"
-
 
 using xdr::operator==;
 
